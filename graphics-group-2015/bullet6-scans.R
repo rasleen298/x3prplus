5713a880c1c60611d7eab627152aacd0b7561231
Eric Hare <ericrhare@gmail.com>
2016-02-09 08:10:03 -0600

    Include the presentations

---
+++ graphics-group-2015/bullet6-scans.R
@@ -0,0 +1,23 @@
+i <- 1
+bname <- sprintf("../Hamby252_3DX3P1of2/Br6 Bullet 1-%d.x3p", i)
+b61 <-read.x3p(bname)
+plot3D.x3p.file(b61)
+
+
+i <- 2
+bname <- sprintf("../Hamby252_3DX3P1of2/Br6 Bullet 1-%d.x3p", i)
+b61 <-read.x3p(bname)
+plot3D.x3p.file(b61)
+
+i <- 1
+bname <- sprintf("../Hamby252_3DX3P1of2/Br6 Bullet 2-%d.x3p", i)
+b62 <-read.x3p(bname)
+plot3D.x3p.file(b62)
+
+
+i <- 2
+bname <- sprintf("../Hamby252_3DX3P1of2/Br6 Bullet 2-%d.x3p", i)
+b62 <-read.x3p(bname)
+plot3D.x3p.file(b62)
+
+
